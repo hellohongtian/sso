fb73f660a0dbf37d44701a2a7b55a8df84b2f3c2
Hongtian Li <lihongtian@xin.com>
2019-08-28 21:29:15 +0800

    test

---
+++ src/Sso.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hongtianli
+ * Date: 2019/8/28
+ * Time: 2:04 PM
+ */
+
+namespace Sso;
+
+
+class Sso
+{
+
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * 获取映射关系
+     *
+     */
+    public function get_map_relation()
+    {
+
+    }
+
+    /**
+     * 账号添加
+     * @return array
+     */
+    public function add_account()
+    {
+        return [];
+    }
+
+    /**
+     * 账号编辑
+     * @return array
+     */
+    public function edit_account()
+    {
+        return [];
+    }
+
+}
